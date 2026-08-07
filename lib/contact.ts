c4759aa116b6e10457b7a6f10b0a386e4b4c872a
shin <kunkun9411@gmail.com>
2026-08-07 15:42:46 +0200

    Use Duo Nerds Discord invite

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,3 +1,3 @@
-export const discordUrl = process.env.NEXT_PUBLIC_DISCORD_URL || 'https://discord.com/app';
+export const discordUrl = process.env.NEXT_PUBLIC_DISCORD_URL || 'https://discord.gg/225zd5PS9y';
+export const discordInviteUrl = process.env.NEXT_PUBLIC_DISCORD_INVITE_URL || 'https://discord.gg/225zd5PS9y';
 export const contactEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'contact@duonerds.online';
-
