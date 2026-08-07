3bbd6d50e478b9aaed0f8786e88043feddb81b4f
shin <kunkun9411@gmail.com>
2026-08-07 15:21:26 +0200

    Add work samples and company pages

---
+++ lib/contact.ts
@@ -0,0 +1,3 @@
+export const discordUrl = process.env.NEXT_PUBLIC_DISCORD_URL || 'https://discord.com/app';
+export const contactEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'contact@duonerds.online';
+
