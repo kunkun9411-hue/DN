71bc502fa99b85706114d69b4124a1a9466291e6
shin <kunkun9411@gmail.com>
2026-08-07 15:21:19 +0200

    Add work samples and company pages

---
+++ app/ueber-uns/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import { AboutPage } from '@/components/company-pages';
+
+export const metadata: Metadata = { title: 'Ãœber DuoNerds' };
+
+export default function AboutRoute() {
+  return <AboutPage />;
+}
+
