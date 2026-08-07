01f5eece6cf2f2ae1d0e56525ab8af1d2ebcf2f6
shin <kunkun9411@gmail.com>
2026-08-07 14:46:47 +0200

    Add customer portal foundation

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import { AuthForm } from '@/components/portal';
+
+export const metadata: Metadata = { title: 'Portal Login' };
+
+export default function LoginPage() {
+  return <AuthForm mode="login" />;
+}
+
