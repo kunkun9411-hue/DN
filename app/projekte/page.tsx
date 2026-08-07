b9973e8e4e7ad153dbbca897db427b17498e661b
shin <kunkun9411@gmail.com>
2026-08-07 15:21:16 +0200

    Add work samples and company pages

---
+++ app/projekte/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import { ProjectsPage } from '@/components/project-sections';
+
+export const metadata: Metadata = { title: 'Arbeitsproben' };
+
+export default function ProjectsRoute() {
+  return <ProjectsPage />;
+}
+
