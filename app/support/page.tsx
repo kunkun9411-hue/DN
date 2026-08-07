2008eff358f5ac25772ca46c66efb1ef9defab6c
shin <kunkun9411@gmail.com>
2026-08-07 15:21:18 +0200

    Add work samples and company pages

---
+++ app/support/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import { SupportPage } from '@/components/company-pages';
+
+export const metadata: Metadata = { title: 'Support & Wartung' };
+
+export default function SupportRoute() {
+  return <SupportPage />;
+}
+
